7d389f2de7214dae0cd3a24ebee55ef3342dc229
Ran Kim <rannio@gmail.com>
2022-02-25 11:35:25 -0500

    learned intro to DOM

---
+++ app.js
@@ -0,0 +1,15 @@
+//document.querySelector('h1').innerText = 'Welcome to the Upside Down';
+
+//const image = document.querySelector('img')
+
+const eggos = document.createElement('img')
+
+eggos.setAttribute('src', 'https://i.ytimg.com/vi/TgSmPqMGk2g/maxresdefault.jpg')
+
+
+document.querySelector('.container').appendChild(eggos)
+
+eggos.style.width = '50%'
+
+eggos.remove()
+
